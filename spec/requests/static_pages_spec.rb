ee68fa71d4822760fb1e87097523bfdf1d858dd5
ChoongHoe, Chow <choonghoe@gmail.com>
2012-02-24 22:15:52 +0800

    added css layout, re-confgured routing and removed index page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,41 +4,41 @@ describe "StaticPages" do
   let (:base_title) {"Ruby on Rails Tutorial Sample App"}
   describe "Home Page" do
     it "should have the content h1 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'Sample App')
     end
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', :text => "#{base_title} | Home")
     end
   end
   describe "Help Page" do
     it "should have the content h1 'Help'" do
-      visit '/static_pages/help'
+      visit help_path 
       page.should have_selector('h1', :text=>'Help')
     end
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
   describe "About Page" do
     it "should have the content h1 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title', :text => "#{base_title} | About Us")
     end
   end 
   describe "Contact Page" do
     it "should have the content h1 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', :text => 'Contact')
     end
     it "should have the right title" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title', :text => "#{base_title} | Contact")
     end
   end 
